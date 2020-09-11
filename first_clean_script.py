4fdb028bd0562da144a9a4de9af36034b83027ff
Alyssa Bulatek <abulatek@ufl.edu>
2020-09-11 16:13:27 -0500

    Added full file path to imagename

--- first_clean_script.py
+++ first_clean_script.py
@@ -1,3 +1,3 @@
 full_path = '/orange/adamginsburg/brick_alma_linesurvey/2019.1.00092.S/science_goal.uid___A001_X1465_X3a31/group.uid___A001_X1465_X3a32/member.uid___A001_X1465_X3a33/calibrated/calibrated_final.ms'
 
-tclean(vis=full_path, imagename='source_first_test_ab_dirty', field='2', spw='', specmode='cube', gridder='standard', cell=['0.2arcsec'], imsize=[512,512], weighting='natural', threshold='0.0mJy', interactive=False)
+tclean(vis=full_path, imagename='/orange/adamginsburg/brick_alma_linesurvey/2019.1.00092.S/imaging_results/source_first_test_ab_dirty', field='2', spw='', specmode='cube', gridder='standard', cell=['0.2arcsec'], imsize=[512,512], weighting='natural', threshold='0.0mJy', interactive=False)
