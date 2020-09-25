c5c61c872d4801417ea7ae2888751e9c921e046c
Alyssa Bulatek <abulatek@ufl.edu>
2020-09-25 13:12:27 -0500

    Fixed incorrect call to xmatch catalog in last line

--- bubble/vizier_xmatch_VVV_GDR2.py
+++ bubble/vizier_xmatch_VVV_GDR2.py
@@ -16,4 +16,4 @@ print("The VVV catalog around the Brick has this many sources:", len(VVV_B))
 # Cross-match the GALACTICNUCLEUS survey with Gaia DR2
 VVV_GDR2_xmatch = XMatch.query(cat1=VVV_B, cat2='vizier:I/345/gaia2', max_distance=1*u.arcsec, colRA1='RAJ2000', colDec1='DEJ2000', colRA2='RA_ICRS', colDec2='DE_ICRS')
 print("The VVV/GDR2 crossmatch catalog has this many sources:", len(VVV_GDR2_xmatch))
-print("That means there are this many sources in VVV that aren't in GDR2:", len(VVV_B) - len(GN_GDR2_xmatch))
+print("That means there are this many sources in VVV that aren't in GDR2:", len(VVV_B) - len(VVV_GDR2_xmatch))
