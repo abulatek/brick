27916d425ed710c9502d9bff106dbfb47d58cc2c
Alyssa Bulatek <abulatek@ufl.edu>
2020-10-21 11:41:29 -0500

    Add files via upload

---
+++ 138_spw25_masking.py
@@ -0,0 +1,42 @@
+## Pylab requires this to have a visual component, so if using pylab, this should be ran from an xpra GUI session
+from spectral_cube import SpectralCube
+from astropy import units as u
+
+# Use the code below to mask the cube at a certain sigma threshold
+cube = SpectralCube.read('source_ab_138_spw25_dirty_512.image')
+mask = cube > 7.0*u.mJy/u.beam
+
+from casatools import image
+ia = image()
+boolmask = mask.include().compute()
+
+# Use the code below to output a mask file
+ia.open('source_ab_138_spw25_dirty_512.image')
+cs = ia.coordsys()
+ia.close()
+
+# ia.fromarray(outfile='source_ab_138_spw25_dirty_512.mask', pixels=boolmask.astype('float')[:,None,:,:].T, csys=cs.torecord(), overwrite=True)
+# ia.close()
+
+# Use the code below to visualize what the cube looks like without its circular mask
+# import pylab as pl; pl.ion(); pl.draw(); pl.show()
+# unmasked = cube.with_mask(np.ones(cube.shape, dtype='bool'), inherit_mask=False)
+# Above sometimes crashes Python. Let's try: 
+unmasked = cube.with_mask(cube=cube, inherit_mask=False)
+
+# Use the code below to visualize the erosion and dilation
+import scipy.ndimage
+# x = boolmask[2228] 
+# pl.figure()
+# pl.imshow(x)
+# y = scipy.ndimage.binary_erosion(x, iterations=2)
+# pl.imshow(y)
+# z1 = scipy.ndimage.binary_dilation(scipy.ndimage.binary_erosion(x, iterations=2), iterations=2)
+# pl.imshow(z1)
+# z2 = scipy.ndimage.binary_dilation(scipy.ndimage.binary_erosion(x, iterations=3), iterations=4)
+# pl.imshow(z2)
+
+# Use the code below to output an eroded/dilated mask
+boolmask_e3_d4 = scipy.ndimage.binary_dilation(scipy.ndimage.binary_erosion(boolmask, iterations=3), iterations=4)
+ia.fromarray(outfile='source_ab_138_spw25_dirty_512_e3_d4.mask', pixels=boolmask_e3_d4.astype('float')[:,None,:,:].T, csys=cs.torecord(), overwrite=True)
+ia.close()
