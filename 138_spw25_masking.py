e9ace9658b7d9480bfa4ce6f42b23e00284c2dba
Alyssa Bulatek <abulatek@ufl.edu>
2020-10-21 19:00:41 -0500

    Added print statements to track progress through script
    Also changed argument of `unmasked = cube.with_mask(cube == cube, inherit_mask=False)` to be `cube == cube`

--- 138_spw25_masking.py
+++ 138_spw25_masking.py
@@ -2,6 +2,7 @@
 from spectral_cube import SpectralCube
 from astropy import units as u
 
+print("Now masking the cube at sigma threshold")
 # Use the code below to mask the cube at a certain sigma threshold
 cube = SpectralCube.read('source_ab_138_spw25_dirty_512.image')
 mask = cube > 7.0*u.mJy/u.beam
@@ -10,20 +11,23 @@ from casatools import image
 ia = image()
 boolmask = mask.include().compute()
 
+print("Now outputting the mask file")
 # Use the code below to output a mask file
 ia.open('source_ab_138_spw25_dirty_512.image')
 cs = ia.coordsys()
 ia.close()
 
-# ia.fromarray(outfile='source_ab_138_spw25_dirty_512.mask', pixels=boolmask.astype('float')[:,None,:,:].T, csys=cs.torecord(), overwrite=True)
-# ia.close()
+ia.fromarray(outfile='source_ab_138_spw25_dirty_512.mask', pixels=boolmask.astype('float')[:,None,:,:].T, csys=cs.torecord(), overwrite=True)
+ia.close()
 
-# Use the code below to visualize what the cube looks like without its circular mask
-# import pylab as pl; pl.ion(); pl.draw(); pl.show()
+print("Now visualizing masked cube without the circular vignette")
+# Use the code below to visualize what the cube looks like without its circular vignette
+import pylab as pl; pl.ion(); pl.draw(); pl.show()
 # unmasked = cube.with_mask(np.ones(cube.shape, dtype='bool'), inherit_mask=False)
 # Above sometimes crashes Python. Let's try: 
-unmasked = cube.with_mask(cube=cube, inherit_mask=False)
+unmasked = cube.with_mask(cube == cube, inherit_mask=False)
 
+# print("Now visualizing the erosion and dilation")
 # Use the code below to visualize the erosion and dilation
 import scipy.ndimage
 # x = boolmask[2228] 
@@ -36,7 +40,9 @@ import scipy.ndimage
 # z2 = scipy.ndimage.binary_dilation(scipy.ndimage.binary_erosion(x, iterations=3), iterations=4)
 # pl.imshow(z2)
 
+print("Now calculating the eroded/dilated mask")
 # Use the code below to output an eroded/dilated mask
 boolmask_e3_d4 = scipy.ndimage.binary_dilation(scipy.ndimage.binary_erosion(boolmask, iterations=3), iterations=4)
+print("Now outputting the eroded/dilated mask")
 ia.fromarray(outfile='source_ab_138_spw25_dirty_512_e3_d4.mask', pixels=boolmask_e3_d4.astype('float')[:,None,:,:].T, csys=cs.torecord(), overwrite=True)
 ia.close()
