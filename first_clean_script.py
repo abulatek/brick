bec840ccce182adaebfeca158583e09216f1fcd6
Alyssa Bulatek <abulatek@login3.ufhpc>
2020-10-22 22:06:17 -0400

    deleting old files before adding updated project

--- first_clean_script.py
+++
@@ -1,3 +0,0 @@
-full_path = '/blue/adamginsburg/abulatek/brick/symlinks/data_31/calibrated_final.ms'
-
-tclean(vis=full_path, imagename='/blue/adamginsburg/abulatek/brick/symlinks/imaging_results/source_ab_138_spw25_dirty_512', field='2', spw='25', specmode='cube', gridder='standard', cell=['0.2arcsec'], imsize=[512,512], weighting='natural', threshold='0.0mJy', interactive=False, chanchunks=-1)
