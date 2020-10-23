ca8895d99e6541ef787f58394cdca1d1576ee0cf
Alyssa Bulatek <abulatek@login1.ufhpc>
2020-10-23 11:11:43 -0400

    Rewrote clean scripts (.py and .sh) and reorganized log files

--- clean_script.sh
+++ clean_script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=first_clean
+#SBATCH --job-name=ab_138_spw25_2sigma_clean
 #SBATCH --mail-user=abulatek@ufl.edu
 #SBATCH --mail-type=FAIL,END
-#SBATCH --output first_clean_%j.log
+#SBATCH --output ab_138_spw25_2sigma_clean_%j.log
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --mem=20gb
-#SBATCH --time=00:30:00
+#SBATCH --mem=32gb
+#SBATCH --time=24:00:00
 #SBATCH --qos=adamginsburg-b
 pwd; hostname; date
 
@@ -14,5 +14,5 @@ echo $LOGFILENAME
 
 export CASA=/blue/adamginsburg/adamginsburg/casa/casa-release-5.7.0-134.el7/bin/casa
 
-xvfb-run -d ${CASA} --logfile=${LOGFILENAME} --nogui --nologger -c "execfile('/blue/adamginsburg/abulatek/brick/first_clean_script.py')"
+xvfb-run -d ${CASA} --logfile=${LOGFILENAME} --nogui --nologger -c "execfile('/blue/adamginsburg/abulatek/brick/clean_script.py')"
 
