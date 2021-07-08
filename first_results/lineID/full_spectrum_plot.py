3f8405c28f685776fca48b4467dbf28bd99c7b82
Alyssa Bulatek <abulatek@login2.ufhpc>
2021-07-08 18:03:39 -0400

    Updated plots and plotting for line IDs

--- first_results/lineID/full_spectrum_plot.py
+++ first_results/lineID/full_spectrum_plot.py
@@ -5,7 +5,7 @@ from spectral_cube import OneDSpectrum
 from astropy.io import fits
 from astropy import units as u
 
-def plot_spectrum(fn, xmin=None, xmax=None):
+def plot_spectrum(fn, save=False):
 #     pl.close(1)
     pl.close(2)
     
@@ -14,16 +14,17 @@ def plot_spectrum(fn, xmin=None, xmax=None):
 #     spectrum.plotter(figure=fig)
 #     pl.show()
 
-    fig2=pl.figure(2, figsize=(10,5))
+    fig2=pl.figure(2, figsize=(12,5))
     kspectrum = OneDSpectrum.from_hdu(fits.open(fn)).to(u.K)
     kspectrum_ps = pyspeckit.Spectrum.from_hdu(kspectrum.hdu)
-    if xmin==None and xmax==None:
-        kspectrum_ps.plotter(figure=fig2)
-    else:
-        kspectrum_ps.plotter(figure=fig2, xmin=xmin, xmax=xmax)
+    kspectrum_ps.xarr.convert_to_unit('GHz')
+    kspectrum_ps.plotter(figure=fig2)
     pl.show()
+    if save == True:
+        filename = 'test_spectrum.png'
+        kspectrum_ps.plotter.figure.savefig(filename, dpi=200, bbox_inches='tight')
 
-def plot_spectrum_line_ids(fn, ids, xmin=None, xmax=None):
+def plot_spectrum_line_ids(fn, ids):
 #     pl.close(1)
     pl.close(2)
     
@@ -38,13 +39,8 @@ def plot_spectrum_line_ids(fn, ids, xmin=None, xmax=None):
     kspectrum = OneDSpectrum.from_hdu(fits.open(fn)).to(u.K)
     kspectrum_ps = pyspeckit.Spectrum.from_hdu(kspectrum.hdu)
     kspectrum_ps.plotter(figure=fig2)
-    if xmin==None and xmax==None:
-        kspectrum_ps.plotter.line_ids(figure=fig2, line_names=ids['Species'], # ChemicalName 
-                                        line_xvals=ids['Freq'], xval_units='GHz', plot_kwargs={'color':'silver'})
-    else:
-        kspectrum_ps.plotter.line_ids(figure=fig2, line_names=ids['Species'], # ChemicalName 
-                                        line_xvals=ids['Freq'], xval_units='GHz', plot_kwargs={'color':'silver'},
-                                        xmin=xmin, xmax=xmax)
+    kspectrum_ps.plotter.line_ids(figure=fig2, line_names=ids['Species'], # ChemicalName 
+                                  line_xvals=ids['Freq'], xval_units='GHz', plot_kwargs={'color':'silver'})
     pl.show()
     
 def plot_spectrum_line_ids_final(fn, ids, save=False):    
@@ -61,9 +57,11 @@ def plot_spectrum_line_ids_final(fn, ids, save=False):
     fig2=pl.figure(2, figsize=(12,5))
     kspectrum = OneDSpectrum.from_hdu(fits.open(fn)).to(u.K)
     kspectrum_ps = pyspeckit.Spectrum.from_hdu(kspectrum.hdu)
+    kspectrum_ps.xarr.convert_to_unit('GHz')
     kspectrum_ps.plotter(figure=fig2)
     kspectrum_ps.plotter.line_ids(figure=fig2, line_names=ids['Species'], # ChemicalName 
-                                  line_xvals=ids['Frequency (GHz)'], xval_units='GHz', 
+                                  line_xvals=ids['Frequency (GHz)'] - (ids['Frequency (GHz)']*(ids['Velocity (km/s)']/299792.)),
+                                  xval_units='GHz', 
                                   plot_kwargs={'color':'silver'})
     pl.show()
     if save == True:
